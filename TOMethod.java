1ebd4e4e4ad7dc835ceda53a3822930768156f7b
Trader Oracle <157963314+TraderOracle@users.noreply.github.com>
2025-03-16 18:33:38 -0500

    Added volume imbalances
    Added volume imbalances

--- TOMethod.java
+++ TOMethod.java
@@ -35,7 +35,7 @@ public class TOMethod extends Study
     private static final Color GREEN = new Color(0, 255, 0);
     private static final Color WHITE = new Color(255, 255, 255);
     private static final Color YELLOW = new Color(255, 0, 0);
-    private List<Double> al = new ArrayList<>();
+    private List<Line> al = new ArrayList<>();
 
     public String getHTML(String urlToRead) throws Exception {
         StringBuilder result = new StringBuilder();
@@ -48,14 +48,7 @@ public class TOMethod extends Study
                 result.append(line);
             }
         }
-
         return result.toString();
-    }
-
-    @Override
-    public void onLoad(Defaults defaults)
-    {
-
     }
 
     @Override
@@ -70,18 +63,11 @@ public class TOMethod extends Study
         var tab=sd.addTab("General");
         var grp=tab.addGroup("");
         grp.addRow(new PathDescriptor(Inputs.PATH, "Line", defaults.getLineColor(), 2.0f, null, true, false, true));
-        grp.addRow(new BooleanDescriptor("extRight", "Extend Right", false));
-        grp.addRow(new BooleanDescriptor("extLeft", "Extend Left", false));
+        grp.addRow(new InputDescriptor(Inputs.INPUT, get("Input"), Enums.BarInput.CLOSE));
 
         var desc = createRD();
-        // Signals
         desc.declareSignal(Signals.BOINK, "BOINK");
         desc.declareSignal(Signals.ENG_BB, "Engulfing candle off BB");
-
-        var grp1 = tab.addGroup(get("LBL_INPUTS"));
-        grp1.addRow(new InputDescriptor(Inputs.INPUT, get("Input"), Enums.BarInput.CLOSE));
-        grp1.addRow(new IntegerDescriptor(Inputs.PERIOD, get("Period"), 20, 1, 9999, 1));
-        grp1.addRow(new PathDescriptor(Inputs.PATH, "Line", defaults.getLineColor(), 3.0f, null, true, false, true));
     }
 
     @Override
@@ -102,6 +88,8 @@ public class TOMethod extends Study
         //region CANDLE CALCS
         double close = series.getClose(index);
         double open =  series.getOpen(index);
+        double pclose = series.getClose(index - 1);
+        double popen =  series.getOpen(index - 1);
         double clow = series.getLow(index);
         double plow = series.getLow(index - 1);
         double high = series.getHigh(index);
@@ -142,6 +130,39 @@ public class TOMethod extends Study
         }
         */
 
+        // =-=-=-=-=   VOLUME IMBALANCES GREEN   =-=-=-=-=
+        if (c0G && c1G && open > pclose)
+        {
+            int iStart = series.getStartIndex();
+            int iEnd = series.getEndIndex();
+            Coordinate coords = new Coordinate(series.getStartTime(index), (double) open);
+            Coordinate coordsEnd = new Coordinate(series.getStartTime(iEnd), (double) open);
+            // PathInfo(Color c, float width, float[] dash, boolean enabled, boolean continuous, boolean showBars, int barCenter, Integer fixedWidth)
+            PathInfo pf = new PathInfo(new Color(126, 179, 252), 2, new float[] {4,1,3}, true, true, false, 0, 2);
+            Line lk = new Line(coords, coordsEnd, pf);
+            // lk.setText(Math.abs(open), new Font("Arial", Font.PLAIN, 12));
+            if (!al.contains(lk))
+                al.add(lk);
+            this.addFigure(lk);
+        }
+
+        // =-=-=-=-=   VOLUME IMBALANCES RED   =-=-=-=-=
+        if (c0R && c1R && open < pclose)
+        {
+            int iStart = series.getStartIndex();
+            int iEnd = series.getEndIndex();
+            Coordinate coords = new Coordinate(series.getStartTime(index), (double) close);
+            Coordinate coordsEnd = new Coordinate(series.getStartTime(iEnd), (double) close);
+            // PathInfo(Color c, float width, float[] dash, boolean enabled, boolean continuous, boolean showBars, int barCenter, Integer fixedWidth)
+            PathInfo pf = new PathInfo(new Color(126, 179, 252), 2, new float[] {4,1,3}, true, true, false, 0, 2);
+            Line lk = new Line(coords, coordsEnd, pf);
+            // lk.setText(Math.abs(open), new Font("Arial", Font.PLAIN, 12));
+            if (!al.contains(lk))
+                al.add(lk);
+            this.addFigure(lk);
+        }
+
+        // =-=-=-=-=   ENGULFING LOW   =-=-=-=-=
         if ((clow < lowerBB || plow < lowerBB) && body > pbody && c1R && c0G)
         {
             Coordinate coords = new Coordinate(series.getStartTime(index), (double) series.getLow(index) - 2);
@@ -151,15 +172,57 @@ public class TOMethod extends Study
             return;
         }
 
+        // =-=-=-=-=   ENGULFING HIGH   =-=-=-=-=
         if ((high > upperBB || phigh > upperBB) && body > pbody && c1G && c0R)
         {
+            /*
+            int iStart = series.getStartIndex();
+            int iEnd = series.getEndIndex();
             Coordinate coords = new Coordinate(series.getStartTime(index), (double) series.getHigh(index) + 2);
+            Coordinate coordsEnd = new Coordinate(series.getStartTime(index-100), (double) series.getHigh(index) + 2.2);
             this.addFigure(new Marker(coords, Enums.MarkerType.CIRCLE, Enums.Size.MEDIUM, Enums.Position.TOP, RED, RED));
+
+            Box bx = new Box(coords, coordsEnd);
+            bx.setFillColor(RED);
+            bx.setLineColor(WHITE);
+            //this.addFigure(bx);
+
+            Line lk = new Line(coords, coordsEnd);
+            lk.setColor(WHITE);
+            this.addFigure(lk);
+*/
             ctx.signal(index, Signals.ENG_BB, "ENG_BB", close);
             series.setPriceBarColor(index, WHITE);
             return;
         }
 
+        // clear(): Removes all of the elements from this list.
+        // add(element): Appends the specified element to the end of the list.
+        // remove(element): Removes the first occurrence of the specified element from this list, if it is present.
+        // contains(element): Returns true if this list contains the specified element.
+        //debug("line before = " + al.size());
+
+        for (Line line : al) {
+            boolean bGStop = c0G && close > line.getEndValue() && open < line.getEndValue();
+            boolean bG1Stop = c0G && open > line.getEndValue() && clow < line.getEndValue();
+            boolean bG2Stop = false; // c0G && close > line.getEndValue() && high > line.getEndValue();
+            boolean bRStop = c0R && close < line.getEndValue() && open > line.getEndValue();
+            boolean bR1Stop = c0R && clow < line.getEndValue() && close > line.getEndValue();
+            int idxLine = series.findIndex(line.getEndTime());
+            if (idxLine > index && (bGStop || bRStop || bG1Stop || bR1Stop || bG2Stop))
+            {
+                line.setEnd(series.getStartTime(index), line.getEndValue());
+                this.addFigure(line);
+                break;
+                // setEnd(long time, double value) Sets the location of the end of the line.
+                // double getEndValue() - Gets the value of the ending location of the line.
+
+            }
+            //al.remove(item);
+        }
+        //debug("line after = " + al.size());
+
+        /*
         LineInfo lf = new LineInfo(19680d, Color.red, 2.0f, new float[] {3,3});
         RuntimeDescriptor ts = new RuntimeDescriptor();
         setRuntimeDescriptor(ts);
@@ -171,7 +234,7 @@ public class TOMethod extends Study
         for (int i = 0; i < al.size(); i++) {
             //ts.addHorizontalLine(new LineInfo(al.get(i), RED, 2.0f, new float[] {3,3}));
         }
-
+*/
         if (index == series.size() - 1)
             series.setComplete(index);
     }
