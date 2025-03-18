3e5b376d0e765df72baf7364af461392c1ab7062
Trader Oracle <157963314+TraderOracle@users.noreply.github.com>
2025-03-18 14:33:59 -0500

    Added configuration section

--- TOMethod.java
+++ TOMethod.java
@@ -13,12 +13,12 @@ import com.motivewave.platform.sdk.study.*;
 import com.motivewave.platform.sdk.draw.*;
 
 @StudyHeader(
-        namespace="com.mycompany",
-        id="TOMethod",
+        namespace="com.DickInTheSpleen",
+        id="TOMethod2",
         rb="TraderOracle.nls.strings", // locale specific strings are loaded from here
-        name="TraderOracle Method",
+        name="TraderOracle Method2",
         label="TOMethod",
-        desc="TraderOracle Method",
+        desc="TraderOracle Method2",
         menu="TraderOracle",
         overlay=true,
         studyOverlay=true,
@@ -26,9 +26,8 @@ import com.motivewave.platform.sdk.draw.*;
 
 public class TOMethod extends Study
 {
+    //region VARIABLES
     enum Values { MA, MA2, MA3, MA1, MOMENTUM, BB_KC_DIFF, MACD, SIGNAL, HIST }
-    final static String HIST_IND = "histInd"; // Histogram Parameter
-
     enum Signals { ENG_BB, BOINK }
 
     private static final Color RED = new Color(255, 0, 0);
@@ -36,17 +35,41 @@ public class TOMethod extends Study
     private static final Color WHITE = new Color(255, 255, 255);
     private static final Color YELLOW = new Color(255, 0, 0);
     private List<Line> al = new ArrayList<>();
+    //endregion
 
     @Override
     public void initialize(Defaults defaults)
     {
         var sd=createSD();
-        var tab=sd.addTab("General");
-        var grp=tab.addGroup("");
-        grp.addRow(new PathDescriptor(Inputs.PATH, "Line", defaults.getLineColor(), 2.0f, null, true, false, true));
-        grp.addRow(new InputDescriptor(Inputs.INPUT, get("Input"), Enums.BarInput.CLOSE));
+        var tab=sd.addTab("BOINK");
+        var grp=tab.addGroup("Inputs");
+        grp.addRow(new BooleanDescriptor("ShowBOINK", "Show BOINKs", true));
+        grp.addRow(new BooleanDescriptor("UseKAMA", "Use KAMA", true));
+        grp.addRow(new IntegerDescriptor("KAMAPeriod", "KAMA Period", 9, 1, 9999, 1));
+        grp.addRow(new BooleanDescriptor("StdEMA", "Use Standard EMA", false));
+        grp.addRow(new IntegerDescriptor("EMAPeriod", "EMA Period", 21, 1, 9999, 1));
+        grp.addRow(new BooleanDescriptor("BigEMA", "Use Larger Ema", true));
+        grp.addRow(new IntegerDescriptor("LargerEMAPeriod", "Larger EMA Period", 200, 1, 9999, 1));
 
-        var desc = createRD();
+        grp = tab.addGroup("Markers");
+        grp.addRow(new MarkerDescriptor("UPBOINKMarker", "Up Marker", Enums.MarkerType.TRIANGLE, Enums.Size.SMALL, defaults.getGreen(), defaults.getLineColor(), true, true));
+        grp.addRow(new MarkerDescriptor("DOWNBOINKMarker", "Down Marker", Enums.MarkerType.TRIANGLE, Enums.Size.SMALL, defaults.getRed(), defaults.getLineColor(), true, true));
+
+        var tab1=sd.addTab("Engulfing BB");
+        var grp1=tab1.addGroup("Inputs");
+        grp1.addRow(new BooleanDescriptor("ShowEngBB", "Show Engulfing off Bollinger Band", true));
+        grp1.addRow(new BooleanDescriptor("ColorCandle", "Color candle white", true));
+
+        grp1 = tab1.addGroup("Markers");
+        grp1.addRow(new MarkerDescriptor("UPEngBBMarker", "Up Marker",
+           Enums.MarkerType.CIRCLE, Enums.Size.SMALL,
+           defaults.getGreen(), defaults.getLineColor(), true, true));
+        grp1.addRow(new MarkerDescriptor("DOWNEngBBMarker", "Down Marker",
+           Enums.MarkerType.CIRCLE, Enums.Size.SMALL,
+           defaults.getRed(), defaults.getLineColor(), true, true));
+
+        RuntimeDescriptor desc = new RuntimeDescriptor();
+        setRuntimeDescriptor(desc);
         desc.declareSignal(Signals.BOINK, "BOINK");
         desc.declareSignal(Signals.ENG_BB, "Engulfing candle off BB");
     }
@@ -54,8 +77,6 @@ public class TOMethod extends Study
     @Override
     protected void calculate(int index, DataContext ctx) {
         var series = ctx.getDataSeries();
-
-        //this.addFigure(new Marker(coords, Enums.MarkerType.CIRCLE, Enums.Size.SMALL, Enums.Position.BOTTOM, GREEN, GREEN));
 
         int last = series.size() - 1;
         Object input = getSettings().getInput(Inputs.INPUT);
@@ -67,6 +88,19 @@ public class TOMethod extends Study
         //    series.setComplete(index);
 
         //region CANDLE CALCS
+
+        // GET CONFIG SETTINGS
+        boolean bShowBOINK = getSettings().getBoolean("ShowBOINK");
+        boolean bUseKama = getSettings().getBoolean("KAMAPeriod");
+        boolean bUseStdEMA = getSettings().getBoolean("StdEMA");
+        boolean bUseBigEMA = getSettings().getBoolean("BigEMA");
+        int iKAMAPeriod = getSettings().getInteger("KAMAPeriod");
+        int iEMAPeriod = getSettings().getInteger("EMAPeriod");
+        int iLargerEMAPeriod = getSettings().getInteger("LargerEMAPeriod");
+        boolean bShowEngBB = getSettings().getBoolean("ShowEngBB");
+        boolean bColorCandle = getSettings().getBoolean("ColorCandle");
+
+        // CANDLE CALCULATIONS
         double close = series.getClose(index);
         double open =  series.getOpen(index);
         double pclose = series.getClose(index - 1);
@@ -85,11 +119,15 @@ public class TOMethod extends Study
         boolean c2R = series.getClose(index - 2) < series.getOpen(index - 2);
         double body = Math.abs(open - close);
         double pbody = Math.abs(series.getOpen(index - 1) - series.getClose(index - 1));
-        double kama = series.kama(index, 9, input);
-        double ema200 = series.ema(index, 200, input);
+        double kama = series.kama(index, iKAMAPeriod, input);
+        double ema21 = series.ema(index, iEMAPeriod, input);
+        double ema200 = series.ema(index, iLargerEMAPeriod, input);
         boolean bGDoji = c1G && pbody < phigh - pclose && pbody < popen - plow;
         boolean bRDoji = c1R && pbody < phigh - popen && pbody < pclose - plow;
         boolean bDoji = bGDoji || bRDoji;
+        double myEMA = kama;
+        if (bUseStdEMA)
+            myEMA = ema21;
 
         // Calculate Bollinger Bands
         double middleBB = series.sma(index , 20, input);
@@ -98,31 +136,70 @@ public class TOMethod extends Study
         double lowerBB = middleBB - (2 * stdDev);
         //endregion
 
-        // =-=-=-=-=   BOINK RED   =-=-=-=-=
-        if ((c0R && c1G && high > kama && close < kama && phigh < high))
+        //region BOINKS
+        if (bShowBOINK && (c0R && c1G && high > myEMA && close < myEMA && phigh < high))
         {
+            var marker = getSettings().getMarker("DOWNBOINKMarker");
             Coordinate coords = new Coordinate(series.getStartTime(index), (double) high+1);
-            this.addFigure(new Marker(coords, Enums.MarkerType.TRIANGLE,
-                 Enums.Size.SMALL, Enums.Position.TOP, RED, RED));
-            //series.setPriceBarColor(index, WHITE);
+            this.addFigure(new Marker(coords, Enums.Position.TOP, marker, "Howdy Msg"));
             ctx.signal(index, Signals.BOINK, "BOINK", close);
             return;
         }
 
-        // =-=-=-=-=   BOINK GREEN   =-=-=-=-=
-        if ( (c0G && c1R && clow < kama && close > kama && phigh > high) ||
-             (c0G && bGDoji && clow < kama && close > kama && phigh < kama) ||
-             (c0G && bRDoji && clow < kama && close > kama && plow <= clow) ||
-             (c0G && c1G && c2G && open <= pclose && popen <= ppclose && clow < kama && close > kama))
+        if (bShowBOINK && (c0G && c1R && clow < myEMA && close > myEMA && phigh > high) ||
+             (c0G && bGDoji && clow < myEMA && close > myEMA && phigh < myEMA) ||
+             (c0G && bRDoji && clow < myEMA && close > myEMA && plow <= clow) ||
+             (c0G && c1G && c2G && open <= pclose && popen <= ppclose && clow < myEMA && close > myEMA))
         {
+            var marker = getSettings().getMarker("UPBOINKMarker");
             Coordinate coords = new Coordinate(series.getStartTime(index), (double) clow-1);
-            this.addFigure(new Marker(coords, Enums.MarkerType.TRIANGLE,
-                 Enums.Size.SMALL, Enums.Position.BOTTOM, GREEN, GREEN));
-            //series.setPriceBarColor(index, WHITE);
+            this.addFigure(new Marker(coords, Enums.Position.BOTTOM, marker, "Howdy Msg"));
             ctx.signal(index, Signals.BOINK, "BOINK", close);
             return;
         }
+        //endregion
 
+        //region ENGULFING CANDLE
+        if ((clow < lowerBB || plow < lowerBB) && body > pbody && c1R && bShowEngBB && c0G)
+        {
+            var marker = getSettings().getMarker("UPEngBBMarker");
+            Coordinate coords = new Coordinate(series.getStartTime(index), (double) series.getLow(index) - 1);
+            this.addFigure(new Marker(coords, Enums.Position.BOTTOM, marker, "Howdy Msg"));
+            ctx.signal(index, Signals.ENG_BB, "ENG_BB", close);
+            if (bColorCandle)
+                series.setPriceBarColor(index, WHITE);
+            return;
+        }
+
+        if ((high > upperBB || phigh > upperBB) && body > pbody && bShowEngBB && c1G && c0R)
+        {
+            var marker = getSettings().getMarker("DOWNEngBBMarker");
+            int iStart = series.getStartIndex();
+            int iEnd = series.getEndIndex();
+            Coordinate coords = new Coordinate(series.getStartTime(index), (double) series.getHigh(index) + 1);
+            Coordinate coordsEnd = new Coordinate(series.getStartTime(index-100), (double) series.getHigh(index) + 2.2);
+            this.addFigure(new Marker(coords, Enums.Position.TOP, marker, "Howdy Msg"));
+
+            /*
+            Box bx = new Box(coords, coordsEnd);
+            bx.setFillColor(RED);
+            bx.setLineColor(WHITE);
+            //this.addFigure(bx);
+
+            Line lk = new Line(coords, coordsEnd);
+            lk.setColor(WHITE);
+            this.addFigure(lk);
+*/
+            ctx.signal(index, Signals.ENG_BB, "ENG_BB", close);
+            if (bColorCandle)
+                series.setPriceBarColor(index, WHITE);
+            return;
+        }
+        //endregion
+
+
+        //region VOLUME IMBALANCES
+        /*
         // =-=-=-=-=   VOLUME IMBALANCES GREEN   =-=-=-=-=
         if (c0G && c1G && open > pclose)
         {
@@ -156,47 +233,9 @@ public class TOMethod extends Study
                 al.add(lk);
             //this.addFigure(lk);
         }
-
-        // =-=-=-=-=   ENGULFING LOW   =-=-=-=-=
-        if ((clow < lowerBB || plow < lowerBB) && body > pbody && c1R && c0G)
-        {
-            Coordinate coords = new Coordinate(series.getStartTime(index), (double) series.getLow(index) - 2);
-            this.addFigure(new Marker(coords, Enums.MarkerType.CIRCLE, Enums.Size.MEDIUM, Enums.Position.BOTTOM, GREEN, GREEN));
-            ctx.signal(index, Signals.ENG_BB, "ENG_BB", close);
-            series.setPriceBarColor(index, WHITE);
-            return;
-        }
-
-        // =-=-=-=-=   ENGULFING HIGH   =-=-=-=-=
-        if ((high > upperBB || phigh > upperBB) && body > pbody && c1G && c0R)
-        {
-            int iStart = series.getStartIndex();
-            int iEnd = series.getEndIndex();
-            Coordinate coords = new Coordinate(series.getStartTime(index), (double) series.getHigh(index) + 2);
-            Coordinate coordsEnd = new Coordinate(series.getStartTime(index-100), (double) series.getHigh(index) + 2.2);
-            this.addFigure(new Marker(coords, Enums.MarkerType.CIRCLE, Enums.Size.MEDIUM, Enums.Position.TOP, RED, RED));
-
-            /*
-            Box bx = new Box(coords, coordsEnd);
-            bx.setFillColor(RED);
-            bx.setLineColor(WHITE);
-            //this.addFigure(bx);
-
-            Line lk = new Line(coords, coordsEnd);
-            lk.setColor(WHITE);
-            this.addFigure(lk);
 */
-            ctx.signal(index, Signals.ENG_BB, "ENG_BB", close);
-            series.setPriceBarColor(index, WHITE);
-            return;
-        }
-
-        // clear(): Removes all of the elements from this list.
-        // add(element): Appends the specified element to the end of the list.
-        // remove(element): Removes the first occurrence of the specified element from this list, if it is present.
-        // contains(element): Returns true if this list contains the specified element.
-        //debug("line before = " + al.size());
-
+        //endregion
+        
         for (Line line : al) {
             boolean bGStop = c0G && close > line.getEndValue() && open < line.getEndValue();
             boolean bG1Stop = c0G && open > line.getEndValue() && clow < line.getEndValue();
